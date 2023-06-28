2ac95d4fa9b4302557eceeda84622876352e64eb
Alex Colin <acolin3196@gmail.com>
2023-06-28 15:47:20 -0500

    tag routes get and post routes working

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,18 +3,42 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const productData = await Tag.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const productData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch(error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
